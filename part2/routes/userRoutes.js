90c31ee962362466f8cccbf081b6455806a0d33c
Rimier013 <rimier013f@gmail.com>
2025-06-20 21:43:36 +0000

    [automatic commit] 2025-06-20 21:43:36 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -56,20 +56,6 @@ router.post('/login', async (req, res) => {
 });
 
 
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await db.getUserByUsernameAndPassword(username, password);
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid username or password' });
-    }
-    // Save user info to session
-    req.session.user = { id: user.user_id, username: user.username, role: user.role };
-    res.json({ role: user.role });
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
-  }
-});
 
 
 module.exports = router;
