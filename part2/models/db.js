e3c2c294003842a887b5445193465b32de8da7ee
Rimier013 <rimier013f@gmail.com>
2025-06-20 22:47:47 +0000

    [automatic commit] 2025-06-20 22:47:46 UTC

--- part2/models/db.js
+++ part2/models/db.js
@@ -9,16 +9,16 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-async function getUserByUsernameAndPassword(username, password) {
-  const [rows] = await pool.query(
-    'SELECT * FROM Users WHERE username = ? AND password_hash = ?', [username, password]
-  );
-  return rows[0];
-}
+// async function getUserByUsernameAndPassword(username, password) {
+//   const [rows] = await pool.query(
+//     'SELECT * FROM Users WHERE username = ? AND password_hash = ?', [username, password]
+//   );
+//   return rows[0];
+// }
 
-module.exports = {
-  getUserByUsernameAndPassword,
-  pool
-};
+// module.exports = {
+//   getUserByUsernameAndPassword,
+//   pool
+// };
 
-// module.exports = pool;
+module.exports = pool;
