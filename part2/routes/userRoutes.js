151dd2acb5426eb1d2c528ba59bc2d76ab4a1cac
Rimier013 <rimier013f@gmail.com>
2025-06-21 01:52:49 +0000

    [automatic commit] 2025-06-21 01:52:49 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -117,13 +117,13 @@ router.get('/mydogs', async (req, res) => {
   console.log('Session user:', req.session.user);
 });
 
-router.get('/dogs', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT * FROM Dogs');
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
-});
+// router.get('/dogs', async (req, res) => {
+//   try {
+//     const [rows] = await db.query('SELECT * FROM Dogs');
+//     res.json(rows);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Failed to fetch dogs' });
+//   }
+// });
 
 module.exports = router;
